fbeacaf8fe9ae4af7a6b17181e242e89e61152d0
Roberto Núñez <robertonc1599@gmail.com>
2024-06-02 14:15:49 -0500

    Modifying useMovies Hook

--- src/presentation/hooks/useMovies.ts
+++ src/presentation/hooks/useMovies.ts
@@ -4,23 +4,38 @@ import * as UseCases from '../../core/use-cases';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 
 export const useMovies = () => {
-  const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+  const [popular, setPopular] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
+  const [upComing, setUpComing] = useState<Movie[]>([]);
 
   useEffect(() => {
     initialLoad();
   }, []);
 
   const initialLoad = async () => {
-    const [resp1, resp2, resp3, resp4] = await Promise.all([
-      UseCases.moviesNowPlayingUseCase(movieDBFetcher),
-      UseCases.moviesPopularUseCase(movieDBFetcher),
-      UseCases.moviesTopRatedUseCase(movieDBFetcher),
-      UseCases.moviesUpComingUseCase(movieDBFetcher),
-    ]);
+    const [nowPlayingMovies, popularMovies, topRatedMovies, upComingMovies] =
+      await Promise.all([
+        UseCases.moviesNowPlayingUseCase(movieDBFetcher),
+        UseCases.moviesPopularUseCase(movieDBFetcher),
+        UseCases.moviesTopRatedUseCase(movieDBFetcher),
+        UseCases.moviesUpComingUseCase(movieDBFetcher),
+      ]);
+
+    setNowPlaying(nowPlayingMovies);
+    setPopular(popularMovies);
+    setTopRated(topRatedMovies);
+    setUpComing(upComingMovies);
+
+    setIsLoading(false);
   };
+
   return {
     isLoading,
     nowPlaying,
+    popular,
+    topRated,
+    upComing,
   };
 };
